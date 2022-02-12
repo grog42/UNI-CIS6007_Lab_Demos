8bb82741bc016dc81b48b9c0b5fd1ae123736264
Rohan Collins <41522394+grog42@users.noreply.github.com>
2022-02-12 21:38:52 +0000

    Fixed bug and added comments

--- LabDemo3/LabDemo3.cpp
+++ LabDemo3/LabDemo3.cpp
@@ -6,13 +6,42 @@
 #include <opencv2/opencv.hpp>
 #include <execution>
 #include <future>
+#include <chrono>
 
 using namespace std;
 using namespace cv;
+using namespace std::chrono;
 namespace fs = filesystem;
 
-double PeakSigToNoiseRatio_P(const Mat& img_a, const Mat& img_b) {
+//A serial implmentation of the Peak Signal to noise ratio function
+double PeakSigToNoiseRatio_Ser(const Mat& img_a, const Mat& img_b) {
 
+	//Preload shared resources to minimise execution time
+	const int nChannel = img_a.channels();
+	const uchar* data_a = img_a.data;
+	const uchar* data_b = img_b.data;
+	const int imageArea = img_a.rows * img_a.cols;
+
+	double sum = 0;
+
+	for (int i = 0; i < imageArea; i++) {
+
+		//i maps to pixles of size nChannel
+		int iPtr = i * nChannel;
+		uchar b = data_a[iPtr] - data_b[iPtr];
+		uchar g = data_a[iPtr + 1] - data_b[iPtr + 1];
+		uchar r = data_a[iPtr + 2] - data_b[iPtr + 2];
+
+		sum += pow(r + g + b / 3, 2);
+	}
+
+	return sqrt(sum) / imageArea;
+}
+
+//A parallel implmentation of the Peak Signal to noise ratio function
+double PeakSigToNoiseRatio_Par(const Mat& img_a, const Mat& img_b) {
+
+	//Preload shared resources to minimise execution time
 	int nChannel = img_a.channels();
 	const uchar* data_a = img_a.data;
 	const uchar* data_b = img_b.data;
@@ -20,10 +49,11 @@ double PeakSigToNoiseRatio_P(const Mat& img_a, const Mat& img_b) {
 
 	function<double (int, int, int)> Partition = [&](int start, int end, int depth) {
 
-		if (depth > 9) {
+		if (depth > 8) {
 
 			double sum = 0;
 
+			//i maps to pixles of size nChannel
 			for (int i = start; i < end; i++) {
 				int iPtr = i * nChannel;
 				uchar b = data_a[iPtr] - data_b[iPtr];
@@ -37,12 +67,12 @@ double PeakSigToNoiseRatio_P(const Mat& img_a, const Mat& img_b) {
 		}
 		else
 		{
-			int mid = start + end / 2;
+			int mid = (start + end) / 2;
 
-			auto p1 = Partition(start, mid, ++depth);
-			auto p2 = Partition(mid, end, ++depth);
+			auto p1 = async(Partition, start, mid, ++depth);
+			auto p2 = async(Partition, mid, end, ++depth);
 
-			return p1 + p2;
+			return p1.get() + p2.get();
 		}
 	};
 	
@@ -56,6 +86,7 @@ int main(int argc, char** argv)
 	string baseImgPath = argv[1];
 	string commImgPath = argv[2];
 
+	//Checks performed to ensure both images exist 
 	if (!fs::exists(baseImgPath)) {
 		cout << "Base image not found" << endl;
 		return 0;
@@ -69,14 +100,25 @@ int main(int argc, char** argv)
 	Mat img_a(imread(baseImgPath));
 	Mat img_b(imread(commImgPath));
 
+	//Images must have the same dimentions to be compared
 	if (img_a.rows != img_b.rows || img_a.cols != img_b.cols) {
 		cout << "Images are not comparable" << endl;
 		return 0;
 	}
 
-	double ratio = PeakSigToNoiseRatio_P(img_a, img_b);
+	//Calculate PSNR ratio serialy and log duration
+	auto start_ser = high_resolution_clock::now();
+	double ratio_ser = PeakSigToNoiseRatio_Ser(img_a, img_b);
+	int duration_ser = duration_cast<microseconds>(high_resolution_clock::now() - start_ser).count();
 
-	cout << "Ratio is: " << ratio << endl;
+	//Calculate PSNR ratio in parallel and log duration
+	auto start_par = high_resolution_clock::now();
+	double ratio_par = PeakSigToNoiseRatio_Par(img_a, img_b);
+	int duration_par = duration_cast<microseconds>(high_resolution_clock::now() - start_par).count();
+
+	cout << "Ratio is: " << ratio_ser << endl;
+	cout << "Serial execution time: " << duration_ser << endl;
+	cout << "Parallel execution time: " << duration_par << endl;
 
 	return 0;
 }
